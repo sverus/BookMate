0be143278a8fe0e84984a02ef7ac62868e3401cf
Sammybrother@hotmail.com <Sammybrother@hotmail.com>
2020-09-11 17:06:13 +0930

    manifest update

--- js/main.js
+++ js/main.js
@@ -19,20 +19,15 @@ document.getElementById("submit").addEventListener("click", function() {
             var url = baseUrl + bookValue + key;
             url = url.replace(/ /g, "+");
             console.log(url);
-            //document.getElementById("content").innerHTML = '<p>Search results:</p>';
-
 
             fetch(url)
               .then(response => response.json())
               .then(data => {
                         for (var i = 0; i < data.items.length; i++) {
-                          var item = data.items[i];
-                          // in production code, item.text should have the HTML entities escaped.
+                          var item = data.items[i];                        
                           document.getElementById("content").innerHTML += "<div class="+"book-container>"+"<h3>"+ item.volumeInfo.title+
                           "</h3> <p>Author: "+ item.volumeInfo.authors+ "</p>"+
-                          //"</p> <p>Book Cover:</p>"
                            "<p><img src="+ item.volumeInfo.imageLinks.thumbnail + "></p>"+
-                          //"</p> <p>Book Description: "+ item.volumeInfo.description+ "</p>"+
                           "</p> <p>Category: "+ item.volumeInfo.categories+ "</p>"
                           +"</div>"
               }
