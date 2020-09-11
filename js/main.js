288f03d104cb21d7d8b18bdc242c9034382b86a7
Sammybrother@hotmail.com <Sammybrother@hotmail.com>
2020-09-11 21:43:47 +0930

    adding comments to JS

--- js/main.js
+++ js/main.js
@@ -12,17 +12,19 @@ if ('serviceWorker' in navigator) {
 
 document.getElementById("submit").addEventListener("click", function() {
             event.preventDefault();
-            console.log("Hello");
             var baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
             var bookValue = document.getElementById('bookInput').value;
             var key = "&key=AIzaSyD4GijWnTEom_CThw6R6deoZiI7h6n3UWM";
+            //building API URL
             var url = baseUrl + bookValue + key;
+            //replace spaces with + for API call
             url = url.replace(/ /g, "+");
             console.log(url);
 
             fetch(url)
               .then(response => response.json())
               .then(data => {
+                //loop through search results and display them
                         for (var i = 0; i < data.items.length; i++) {
                           var item = data.items[i];
                           document.getElementById("content").innerHTML += "<div class="+"book-container>"+"<h3>"+ item.volumeInfo.title+
