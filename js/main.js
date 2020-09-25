3b83b0186a755f051875d339386cd8d8551a3dd3
Sammybrother@hotmail.com <Sammybrother@hotmail.com>
2020-09-25 10:40:25 +0930

    added Genre search radiobuttons. further work will allow users to enter their own genre.

--- js/main.js
+++ js/main.js
@@ -10,11 +10,15 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+var baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+var key = "&key=AIzaSyD4GijWnTEom_CThw6R6deoZiI7h6n3UWM";
+
+
 document.getElementById("submit").addEventListener("click", function() {
             event.preventDefault();
-            var baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+
             var bookValue = document.getElementById('bookInput').value;
-            var key = "&key=AIzaSyD4GijWnTEom_CThw6R6deoZiI7h6n3UWM";
+
             //building API URL
             var url = baseUrl + bookValue + key;
             //replace spaces with + for API call
@@ -35,3 +39,33 @@ document.getElementById("submit").addEventListener("click", function() {
               }
       });
 });
+
+
+document.getElementById("submitGenre").addEventListener("click", function() {
+            event.preventDefault();
+            console.log("Hello");
+
+
+            var genreType=document.querySelector('input[name="genre"]:checked').value;
+            console.log(genreType);
+            var url=baseUrl+"subject:"+genreType;
+            console.log(url)
+
+
+            fetch(url)
+              .then(response => response.json())
+              .then(data => {
+                //loop through search results and display them
+                        for (var i = 0; i < data.items.length; i++) {
+                          var item = data.items[i];
+                          document.getElementById("content").innerHTML += "<div class="+"book-container>"+"<h3>"+ item.volumeInfo.title+
+                          "</h3> <p>Author: "+ item.volumeInfo.authors+ "</p>"+
+                           "<p><img src="+ item.volumeInfo.imageLinks.thumbnail + "></p>"+
+                          "</p> <p>Category: "+ item.volumeInfo.categories+ "</p>"
+                          +"</div>"
+              }
+      });  
+
+    }
+
+);
