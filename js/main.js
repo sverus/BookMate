7085f9118f1ff3857ef5db8a253f3c32f2de1de3
Sammybrother@hotmail.com <Sammybrother@hotmail.com>
2020-09-25 12:55:25 +0930

    added author and genre selection- updated fetch link for each. added link for book view. added some styling to seperate search boxes

--- js/main.js
+++ js/main.js
@@ -16,14 +16,26 @@ var key = "&key=AIzaSyD4GijWnTEom_CThw6R6deoZiI7h6n3UWM";
 
 document.getElementById("submit").addEventListener("click", function() {
             event.preventDefault();
-
+            var bookOrAuth = document.querySelector('input[name = bookOrAuthor]:checked').value;
             var bookValue = document.getElementById('bookInput').value;
+            console.log(bookOrAuth);
+            var url = baseUrl + bookValue + key;
+            console.log(url);
+            if(bookOrAuth=='book'){
+              url=baseUrl+"intitle:"+bookValue+key;
+              console.log(url);
+            }
+            else if (bookOrAuth=='author') {
+              url=baseUrl+"inauthor:"+bookValue+key;
+              console.log(url);
+              }
+
 
             //building API URL
-            var url = baseUrl + bookValue + key;
+
             //replace spaces with + for API call
             url = url.replace(/ /g, "+");
-            console.log(url);
+
 
             fetch(url)
               .then(response => response.json())
@@ -34,8 +46,9 @@ document.getElementById("submit").addEventListener("click", function() {
                           document.getElementById("content").innerHTML += "<div class="+"book-container>"+"<h3>"+ item.volumeInfo.title+
                           "</h3> <p>Author: "+ item.volumeInfo.authors+ "</p>"+
                            "<p><img src="+ item.volumeInfo.imageLinks.thumbnail + "></p>"+
-                          "</p> <p>Category: "+ item.volumeInfo.categories+ "</p>"
-                          +"</div>"
+                          "</p> <p>Category: "+ item.volumeInfo.categories+ "</p>"+
+                          "<a href="+ item.volumeInfo.previewLink+ ">View the book</a>"+
+                          "</div>"
               }
       });
 });
