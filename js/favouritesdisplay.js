cdeab1c4a969f2a76e75abd4528f3e5e81809506
Sammybrother@hotmail.com <Sammybrother@hotmail.com>
2020-10-07 14:11:44 +0930

    localstorage data now renders on favourites.html

--- js/favouritesdisplay.js
+++ js/favouritesdisplay.js
@@ -6,28 +6,19 @@ function displayFavourites() {
     var localFavs = [];
     var localFavs = localStorage.getItem("favourites");
     var favouritesToShow = JSON.parse(localStorage.getItem('favourites'));
-    console.log(favouritesToShow);
-
-    // for (var i = 0; i < localFavs.length; i++){
-    //   console.log("each iteration of localFavs", localFavs[i]);
-    // }
+    console.log("favourites", favouritesToShow);
 
 
+    for (var i = 0; i < favouritesToShow.length; i++){
 
-    for (var i = 0; i < localStorage.length; i++){
-      //console.log("each iteration",i);
+console.log(favouritesToShow[i].volumeInfo.title);
+      document.getElementById("favContent").innerHTML += `<div class=" book-container"><div class= "heading"><h3>${favouritesToShow[i].volumeInfo.title}<br /></h3></div>`+
+      `</h3> <p>Author:  ${favouritesToShow[i].volumeInfo.authors} </p>` +
+      `<p><img src= ${favouritesToShow[i].volumeInfo.imageLinks.thumbnail}></p>` +
+      `</p> <p>Category:   ${favouritesToShow[i].volumeInfo.categories}</p>` +
+      `<a href=" ${favouritesToShow[i].volumeInfo.previewLink} ">View the book</a>` +
+      `<input type="submit" class="addToFavs" data-item-id="${favouritesToShow[i].id}"  value="Remove from favourites">`
 
-      document.getElementById("favContent").innerHTML += `<div class=" book-container"><div class= "heading"><h3>${(localStorage.getItem(localStorage.key(i)))}<br /></h3></div>`
 
     }
 }
-
-
-// += `<div class=" book-container"><div class= "heading"><h3>${item.volumeInfo.title}</h3></div>` +
-//     `</h3> <p>Author:  ${item.volumeInfo.authors} </p>` +
-//     `<p><img src= ${item.volumeInfo.imageLinks.thumbnail}></p>` +
-//     `</p> <p>Category:   ${item.volumeInfo.categories}</p>` +
-//     `<a href=" ${item.volumeInfo.previewLink} ">View the book</a>` +
-//     `<input type="submit" class="addToFavs" data-item-id="${item.id}"  value="Add to favourites">` +
-//     `</div>`
-//}
